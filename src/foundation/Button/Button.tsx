4143c07d273b8ed7ea056045aec9842fead236e5
Paul Grau <paul@graycoding.com>
2021-07-21 17:25:06 +0900

    allow override of atomProps

--- src/foundation/Button/Button.tsx
+++ src/foundation/Button/Button.tsx
@@ -1,7 +1,7 @@
-import { composeStyles, styleVariants } from '@vanilla-extract/css';
+import { composeStyles } from '@vanilla-extract/css';
 
 import React from 'react';
-import { atoms } from '../../theme';
+import { atoms, Atoms } from '../../theme';
 
 import * as styles from './styles.css';
 
@@ -9,18 +9,22 @@ import * as styles from './styles.css';
 interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   specialProp: string;
   variant?: keyof typeof styles.variants;
+  atomProps?: Atoms;
+  className?: string;
 }
 
-export function Button({ specialProp, children, variant, ...props }: Props) {
-  const className = composeStyles(
+export function Button({ specialProp, children, variant, atomProps = {}, className = '', ...props }: Props) {
+  const classNames = composeStyles(
     atoms({
       display: 'inline-flex',
       padding: 'medium',
       borderColor: 'gray-700',
+      ...atomProps,
     }),
     styles.base,
     variant ? styles.variants[variant] : '',
+    className,
   );
-  return <button {...props} className={className}>{children}</button>;
+  return <button {...props} className={classNames}>{children}</button>;
 }
 
