07ff9c610f4c15ef7e935c8b6a1a0ebae95b1971
Paul Grau <paul@graycoding.com>
2022-01-27 22:01:44 +0900

    add displayName to box

--- packages/ui/src/foundation/Box/Box.tsx
+++ packages/ui/src/foundation/Box/Box.tsx
@@ -1,5 +1,6 @@
-import { createBox } from 'dessert-box';
+import { createBox } from '@dessert-box/react';
 
 import { atoms } from '../../theme/sprinkles.css';
 
 export const Box = createBox({ atoms });
+Box.displayName = 'Box';
